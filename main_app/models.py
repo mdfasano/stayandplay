b213418a931405ae3db6dbe315aec357c6b0e300
mdfasano <mdfasano16@gmail.com>
2023-10-04 13:38:18 -0400

    formatting and add closing form tags

--- main_app/models.py
+++ main_app/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -12,6 +13,6 @@ class Dog(models.Model):
 
     def __str__(self):
         return f'{self.name} ({self.id})'
-    
+
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
