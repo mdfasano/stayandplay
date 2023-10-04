94230ee899b9e2acf3cb6f7d6affa041e107ba30
Ashley Nicole <ajacobsohn86@users.noreply.github.com>
2023-10-04 16:08:16 -0400

    update and delete dogs

--- main_app/models.py
+++ main_app/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 from django.urls import reverse
-from django.contrib.auth.models import User
+
 
 # Create your models here.
-
 
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
     weight = models.IntegerField()
     notes = models.TextField(max_length=250)
-
-    def __str__(self):
-        return f'{self.name} ({self.id})'
-
-    def get_absolute_url(self):
-        return reverse('detail', kwargs={'dog_id': self.id})
+    
+def __str__(self):
+    return f'{self.name} ({self.id})'
+    
+def get_absolute_url(self):
+    return reverse('detail', kwargs={'dog_id': self.id})
--- main_app/urls.py
+++ main_app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('dogs/<int:dog_id>/', views.dogs_detail, name='detail'),
     path('dogs/create/', views.DogCreate.as_view(), name='dogs_create'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('dogs/<int:pk>/update/', views.DogUpdate.as_view(), name='dogs_update'),
+    path('dogs/<int:pk>/delete/', views.DogDelete.as_view(), name='dogs_delete'),
 ]
